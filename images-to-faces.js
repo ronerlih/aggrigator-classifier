7a3fb75a8e8978bbffaddc430d1f7f46c3cb7ade
Ron <ronerlih@gmail.com>
2018-11-06 19:48:28 -0500

    before Async inputs

--- images-to-faces.js
+++ images-to-faces.js
@@ -31,9 +31,6 @@ axios({
   response.data.pipe(fs.createWriteStream(imageName)).on('close', function(){
   console.log(imageName + " downloaded.");
   
-  //read img to opencv cv mat
-//  const mat = cv.imread('./' + );
-  
   //face detection
 //  cv.imshow('mat', mat);
 //  cv.waitKey();
@@ -75,7 +72,62 @@ axios({
 
 });
 
+//save gentile inputs
+inputs.gentileImages.forEach((url)=>{
+console.log("url: " + url);
 
+// GET request for remote image
+axios({
+  method:'get',
+  url:url,
+  responseType:'stream'
+})
+  .then(function(response) {
+
+  imageName = 'downloaded_gentile/' + url.slice(url.length-8);
+  response.data.pipe(fs.createWriteStream(imageName)).on('close', function(){
+  console.log(imageName + " downloaded.");
+  
+  //face detection
+//  cv.imshow('mat', mat);
+//  cv.waitKey();
+
+	// via Promise
+	cv.imreadAsync('./' +imageName)
+	  .then(mat =>
+		 mat.bgrToGrayAsync()
+			.then(grayImg => classifier.detectMultiScaleAsync(grayImg))
+			.then((res) => {
+			  const { objects, numDetections } = res;
+				//each face
+				
+				res.objects.forEach( face => {
+//				console.log("face: " + JSON.stringify(face,null,2));
+					
+					faceMat = mat.getRegion(new cv.Rect(face.x, face.y, face.width, face.height));
+					faceMat = faceMat.resize(64,64);
+					faceMat.bgrToGrayAsync()
+						.then(gray => { 
+							cv.imwrite('./downloaded_gentile/faces/' + faceCount + '.jpg', gray);
+							faceCount++;}
+							)
+			
+					
+				});
+				})
+	  )
+	  .catch(err => console.error(err));
+
+	
+
+
+  		});
+
+	imageName =  imageName;
+  
+});
+
+});
 //
 //cv.imshow('img', blueMat);
 //cv.waitKey();
